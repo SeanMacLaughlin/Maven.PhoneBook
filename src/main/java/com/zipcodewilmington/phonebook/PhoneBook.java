65466ec6b50204420f479d91f1d1b0e2e3697111
Sean MacLaughlin <seanmaclaughlin@zipcoders-MacBook-Pro.local>
2019-02-16 19:40:54 -0500

    Phone Book V1

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,74 @@
 package com.zipcodewilmington.phonebook;
 
-/**
- * Created by leon on 1/23/18.
- */
-public class PhoneBook {
+import java.util.*;
+
+
+public class PhoneBook
+{
+    private String name;
+    private String number;
+    private String work;
+    private String cell;
+
+    private ArrayList<String> personInfo;
+    public TreeMap<String, ArrayList<String>> phoneBook;
+
+
+    public PhoneBook()
+    {
+        phoneBook = new TreeMap<String, ArrayList<String>>();
+    }
+
+    public ArrayList<String> newPerson(String number, String work, String cell)
+    {
+        ArrayList<String> entries = new ArrayList<String>();
+        entries.add(number);
+        entries.add(work);
+        entries.add(cell);
+
+        return entries;
+    }
+
+    public void add(String name, ArrayList<String> personInfo)
+    {
+        phoneBook.put(name, personInfo);
+    }
+
+    public void remove(String name)
+    {
+        phoneBook.remove(name);
+    }
+
+    public ArrayList<String> lookup(String name)
+    {
+        return phoneBook.get(name);
+    }
+
+    public String reverseLookup(String number)
+    {
+        for(Map.Entry<String, ArrayList<String>> person : phoneBook.entrySet())
+        {
+            if(person.getValue().contains(number))
+            {
+                return person.getKey();
+            }
+        }
+        return "Does not exist";
+    }
+
+    public String display(String name)
+    {
+        for(Map.Entry person : phoneBook.entrySet())
+        {
+            System.out.println(person.getKey() + "  " + person.getValue());
+        }
+        return name;
+    }
+
+    public void removeRecord (String name)
+    {
+    phoneBook.remove(name);
+    }
+
+
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,94 @@
 package com.zipcodewilmington.phonebook;
 
-/**
- * Created by leon on 1/23/18.
- */
-public class PhoneBookTest {
+import org.junit.Assert;
+import org.junit.Test;
+import java.util.ArrayList;
+
+public class PhoneBookTest
+{
+
+    @Test
+    public void addTest()
+    {
+        // Given
+        PhoneBook yellowPages = new PhoneBook();
+        ArrayList<String> person = yellowPages.newPerson("6104446006", "6104441000", "6104446969");
+        int expectedPersons = yellowPages.phoneBook.size() + 1;
+
+        //When
+        yellowPages.add("Ben", person);
+        int actualPersons = yellowPages.phoneBook.size();
+
+        // Then
+        Assert.assertEquals(expectedPersons, actualPersons);
+    }
+
+    @Test
+    public void removeTest()
+    {
+        // Given
+        PhoneBook yellowPages = new PhoneBook();
+        ArrayList<String> person = yellowPages.newPerson("6104446006", "6104441000", "6104446969");
+        yellowPages.add("Ben", person);
+        int expectedPersons = yellowPages.phoneBook.size() - 1;
+
+        // When
+        yellowPages.remove("Ben");
+        int actualPersons = yellowPages.phoneBook.size();
+
+        // Then
+        Assert.assertEquals(expectedPersons, actualPersons);
+    }
+
+    @Test
+    public void lookupTest()
+    {
+        // Given
+        PhoneBook yellowPages = new PhoneBook();
+        ArrayList<String> person = yellowPages.newPerson("6104446006", "6104441000", "6104446969");
+        yellowPages.add("Ben", person);
+
+        // When
+        ArrayList<String> actual = yellowPages.lookup("Ben");
+
+        // Then
+        Assert.assertEquals(person, actual);
+    }
+
+    @Test
+    public void reverseLookupTest()
+    {
+        // Given
+        PhoneBook yellowPages = new PhoneBook();
+        ArrayList<String> person = yellowPages.newPerson("6104446006", "6104441000", "6104446969");
+        yellowPages.add("Ben", person);
+
+        // When
+        String output = yellowPages.reverseLookup("6104446006");
+
+        // Then
+        Assert.assertEquals("Ben", output);
+
+    }
+
+    @Test
+    public void removeRecordTest()
+    {
+        // Given
+        PhoneBook yellowPages = new PhoneBook();
+        ArrayList<String> person = yellowPages.newPerson("6104446006", "6104441000", "6104446969");
+        yellowPages.add("Ben", person);
+        int expectedPersons = yellowPages.phoneBook.size() - 1;
+
+        // When
+        yellowPages.remove("Ben");
+        int actualPersons = yellowPages.phoneBook.size();
+
+        // Then
+        Assert.assertEquals(expectedPersons, actualPersons);
+
+
+
+
+    }
 }
